336e53bea08d5cefa8900c5d36e2950fa1abd4ee
Bruno Santos <bruno.santos1996@gmail.com>
2017-01-23 13:09:32 +0000

    delete item

--- app/script/create.js
+++ app/script/create.js
@@ -2,15 +2,6 @@
 function createDB() {
 	var filebuffer = fs.readFileSync('inventory.db');
 	db = new sql.Database(filebuffer);
-	/*
-	var stmt = db.prepare("INSERT INTO items VALUES (?,?,?,?)");
-	stmt.bind([6,"parafuso 2mm","parafuso",]);
-	stmt.run();
-	
-	var data = db.export();
-	var buffer = new Buffer(data);
-	fs.writeFileSync("inventory.db", buffer);
-	*/
 }
 
 //adds an item to the database
--- app/script/delete.js
+++ app/script/delete.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 	$("#items").on("click", ".delete", function() {
-		console.log("delete");
+		var confirmation = confirm("De certeza que pretende apagar este item?");
+		if (!confirmation) {
+			return;
+		}
+		
+		var id = $(this).parent().siblings()[0].innerHTML;
+		$(this).parent().parent().remove();
+		
+		var submission=db.prepare("DELETE FROM items WHERE id=?");
+		submission.bind([id]);
+		submission.run();
+		
+		var data = db.export();
+		var buffer = new Buffer(data);
+		fs.writeFileSync("inventory.db", buffer);
+		//"refreshes the screen"
+		refreshDB();
 	});
 });
